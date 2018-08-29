efc94bdcd8a78a68728e1440c8c29a2c293381cd
PokelandServer <35106605+PokelandServer@users.noreply.github.com>
2018-08-29 07:03:23 -0700

    Add files via upload

---
+++ WL.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let fs = require('fs');
+let http = require('http');
+const Autolinker = require('autolinker');
+
+let regdateCache = {};
+
+exports.WL = {
+	nameColor: function (name, bold, userGroup) {
+		let userGroupSymbol = Users.usergroups[toId(name)] ? '<b><font color=#948A88>' + Users.usergroups[toId(name)].substr(0, 1) + '</font></b>' : "";
+		return (userGroup ? userGroupSymbol : "") + (bold ? "<b>" : "") + "<font color=" + WL.hashColor(name) + ">" + (Users(name) && Users(name).connected && Users.getExact(name) ? Chat.escapeHTML(Users.getExact(name).name) : Chat.escapeHTML(name)) + "</font>" + (bold ? "</b>" : "");
+	},
+	// usage: WL.nameColor(user.name, true) for bold OR WL.nameColor(user.name, false) for non-bolded.
+
+}
