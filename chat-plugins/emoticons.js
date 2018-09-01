60504520447c8e4107a248759a4c6e9671921323
theodelhay <theodelhay@orange.fr>
2018-09-01 14:46:03 +0200

    Ajout emote

--- chat-plugins/emoticons.js
+++ chat-plugins/emoticons.js
@@ -286,6 +286,7 @@ if (typeof demFeels.extendEmotes === 'function') {
 	':triggered:': 'https://image.noelshack.com/fichiers/2018/30/6/1532787295-bloggif-5b5633650149e.png',
 	'feelsouaf': 'https://cdn.discordapp.com/attachments/434757343943589888/474555786304159744/tenor-1.gif',
 	'#gaspar': 'https://i.imgur.com/6EB30Jt.png',
+	'#selfie': 'http://image.noelshack.com/fichiers/2018/35/6/1535796969-gsapar-balta.png',
 	});
 }
 
