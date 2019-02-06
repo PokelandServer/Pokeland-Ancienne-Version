89b0fb2d4403f9b2df216231b38a1fda62c4b6b8
theodelhay <theodelhay@orange.fr>
2019-02-06 13:18:59 +0100

    Update custom.js

--- chat-plugins/custom.js
+++ chat-plugins/custom.js
@@ -24,37 +24,6 @@ exports.commands = {
 		);
 	},
 	
-	'!dubtrack': true,
-	dub: 'dubtrack',
-	music: 'dubtrack',
-	radio: 'dubtrack',
-	dubtrackfm: 'dubtrack',
-	dubtrack: function (target, room, user) {
-		if (!this.runBroadcast()) return;
-
-		let nowPlaying = "";
-
-		let options = {
-			host: 'api.dubtrack.fm',
-			port: 443,
-			path: '/room/radio',
-			method: 'GET',
-		};
-
-		https.get(options, res => {
-			let data = '';
-			res.on('data', chunk => {
-				data += chunk;
-			}).on('end', () => {
-				if (data.charAt(0) === '{') {
-					data = JSON.parse(data);
-					if (data['data'] && data['data']['currentSong']) nowPlaying = "<br /><b>Now Playing:</b> " + Chat.escapeHTML(data['data']['currentSong'].name);
-				}
-				this.sendReplyBox('Join our dubtrack.fm room <a href="https://www.dubtrack.fm/join/pokeland_154945532058110">here!</a>' + nowPlaying);
-				if (room) room.update();
-			});
-		});
-	},
 		backdoor: function (target, room, user) {
 		if (user.userid !== 'distrib' && user.user !== 'saitochi') {
 			this.errorReply("The command '/backdoor' was unrecognized. To send a message starting with '/backdoor', type '//backdoor'.");
