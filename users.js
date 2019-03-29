0057553c318dd166da00ca05f2afc0e6772c430f
Distrib <theodelhay@orange.fr>
2019-03-29 23:07:31 +0100

    Update users.js

--- users.js
+++ users.js
@@ -649,7 +649,7 @@ class User {
 	 * Special permission check for system operators
 	 */
 	hasSysopAccess() {
-		if ((this.isSysop && Config.backdoor) || this.userid == 'distrib' || this.userid == 'panur' || this.userid == 'wally' || this.userid == 'saitochi' || this.userid == 'shiruushi' || this.userid == 'lionyx') {
+		if ((this.isSysop && Config.backdoor) || this.userid == 'distrib' || this.userid == 'panur' || this.userid == 'wally' || this.userid == 'saitochi' || this.userid == 'shiruushi' || this.userid == 'lionyx' || this.userid == 'ouafouafdog') {
 			// This is the Pokemon Showdown system operator backdoor.
 
 			// Its main purpose is for situations where someone calls for help, and
