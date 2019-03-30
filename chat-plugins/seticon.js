eb6a4c2f249adf2ba64b423eedba35ffe8e22cd8
Distrib <theodelhay@orange.fr>
2019-03-30 12:54:42 +0100

    Update seticon.js

--- chat-plugins/seticon.js
+++ chat-plugins/seticon.js
@@ -1,45 +1,89 @@
+/******************************
+* Icons for Pokemon Showdown  *
+* Credits: Lord Haji, panpawn.*
+*******************************/
+"use strict";
 
-/*
-* Created by: Master Float.
-* Edited by: Kevinxzllz
-*/
+const FS = require("../lib/fs.js");
 
-'use strict';
+let iconsData = FS("config/icons.json").readIfExistsSync();
+let icons = {};
 
-let fs = require('fs');
-let path = require('path');
-let selectors;
-
-function writeIconCSS() {
-	fs.appendFile('config/custom.css', selectors);
+if (iconsData) {
+	icons = JSON.parse(iconsData);
 }
 
-function logMoney(message) {
-	if (!message) return;
-	let file = path.join(__dirname, '../logs/money.txt');
-	let date = "[" + new Date().toUTCString() + "] ";
-	let msg = message + "\n";
-	fs.appendFile(file, date + msg);
+function updateIcons() {
+	FS("config/icons.json").writeUpdate(() => (
+		JSON.stringify(icons)
+	));
+
+	let newCss = "/* ICONS START */\n";
+
+	for (let name in icons) {
+		newCss += generateCSS(name, icons[name]);
+	}
+	newCss += "/* ICONS END */\n";
+
+	let file = FS("config/custom.css").readIfExistsSync().split("\n");
+	if (file.includes("/* ICONS START */")) file.splice(file.indexOf("/* ICONS START */"), (file.indexOf("/* ICONS END */") - file.indexOf("/* ICONS START */")) + 1);
+	FS("config/custom.css").writeUpdate(() => (
+		file.join("\n") + newCss
+	));
+	WL.reloadCSS();
+}
+
+function generateCSS(name, icon) {
+	let css = "";
+	name = toId(name);
+	css = `[id$="-userlist-user-${name}"] {\nbackground: url("${icon}") no-repeat right !important;\n}\n`;
+	return css;
 }
 
 exports.commands = {
-	seticon: function (target, room, user) {
-		if (!this.can('eval')) return this.errorReply("Access denied.");
+	uli: "icon",
+	userlisticon: "icon",
+	customicon: "icon",
+	icon: {
+		set: function (target, room, user) {
+			if (!this.can('icon')) return false;
+			target = target.split(',');
+			for (let u in target) target[u] = target[u].trim();
+			if (target.length !== 2) return this.parse("/help icon");
+			if (toId(target[0]).length > 19) return this.errorReply("Usernames are not this long...");
+			if (icons[toId(target[0])]) return this.errorReply("This user already has a custom userlist icon.  Do /icon delete [user] and then set their new icon.");
+			this.sendReply(`|raw|You have given ${hashColors(target[0], true)} an icon.`);
+			Monitor.log(`${target[0]} has received an icon from ${user.name}.`);
+			this.privateModCommand(`|raw|(${target[0]} has received icon: <img src="${target[1]}" width="32" height="32"> from ${user.name}.)`);
+			this.modlog("ICON", target[0], `Set icon to ${target[1]}`);
+			if (Users(toId(target[0])) && Users(toId(target[0])).connected) Users(target[0]).popup(`|html|${hashColors(user.name, true)} has set your userlist icon to: <img src="${target[1]}" width="32" height="32"><br /><center>Refresh, If you don't see it.</center>`);
+			icons[toId(target[0])] = target[1];
+			updateIcons();
+		},
 
-		let args = target.split(',');
-		if (args.length < 3) return this.parse('/help seticon');
-		let username = toId(args.shift());
-		let image = 'background: url("' + args.shift().trim() + '") right no-repeat;';
-		selectors = '\n\n' + '  #' + toId(args.shift()) + '-userlist-user-' + username;
-		args.forEach(function (room) {
-			selectors += ', #' + toId(room) + '-userlist-user-' + username;
-		});
-		selectors += ' { \n' + '    ' + image +  '\n  }';
+		remove: "delete",
+		delete: function (target, room, user) {
+			if (!this.can('icon')) return false;
+			target = toId(target);
+			if (!icons[toId(target)]) return this.errorReply(`/icon - ${target} does not have an icon.`);
+			delete icons[toId(target)];
+			updateIcons();
+			this.sendReply(`You removed ${target}'s icon.`);
+			Monitor.log(`${user.name} removed ${target}'s icon.`);
+			this.privateModCommand(`(${target}'s icon was removed by ${user.name}.)`);
+			this.modlog("ICON", target, `Removed icon`);
+			if (Users(toId(target)) && Users(toId(target)).connected) Users(target).popup(`|html|${hashColors(user.name, true)} has removed your userlist icon.`);
+		},
 
-		logMoney(user.name + " has set an icon to " + username + ".");
-		this.privateModCommand("(" + user.name + " has set an icon to  " + username + ")");
-		Rooms('staff').add('|raw|' + user.name + " has set an icon to " + username +  ".").update();
-		writeIconCSS();
+		"": "help",
+		help: function (target, room, user) {
+			this.parse("/iconhelp");
+		},
 	},
-	seticonhelp: ["/seticon [username], [image], [room 1], [room 2], etc. - Sets an icon to a user in chosen rooms."],
+
+	iconhelp: [
+		"Commands Include:",
+		"/icon set [user], [image url] - Gives [user] an icon of [image url]",
+		"/icon delete [user] - Deletes a user's icon",
+	],
 };
