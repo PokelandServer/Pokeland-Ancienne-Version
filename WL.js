5e7e156f4503cadc2aeb6fced0ef588cba56ff39
Distrib <theodelhay@orange.fr>
2019-03-30 12:53:54 +0100

    Update WL.js

--- WL.js
+++ WL.js
@@ -2,6 +2,7 @@
 
 let fs = require('fs');
 let http = require('http');
+let https = require('https');
 const Autolinker = require('autolinker');
 
 let regdateCache = {};
@@ -12,5 +13,9 @@ exports.WL = {
 		return (userGroup ? userGroupSymbol : "") + (bold ? "<b>" : "") + "<font color=" + WL.hashColor(name) + ">" + (Users(name) && Users(name).connected && Users.getExact(name) ? Chat.escapeHTML(Users.getExact(name).name) : Chat.escapeHTML(name)) + "</font>" + (bold ? "</b>" : "");
 	},
 	// usage: WL.nameColor(user.name, true) for bold OR WL.nameColor(user.name, false) for non-bolded.
-
+	reloadCSS: function () {
+		const cssPath = 'pokeland'; // This should be the server id if Config.serverid doesn't exist. Ex: 'serverid'
+		let req = https.get('https://play.pokemonshowdown.com/customcss.php?server=' + (Config.serverid || cssPath), () => {});
+		req.end();
+	},
 }
